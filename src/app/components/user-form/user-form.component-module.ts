e6d2d8ea2a4d322648692c6a55ba4fb5a4703cd1
Maciej Paluch <maciekpal@gmail.com>
2022-11-25 17:53:35 +0100

    Exercise 8.2 - Advanced form - radio group
    Changet selection list to radio-group

--- src/app/components/user-form/user-form.component-module.ts
+++ src/app/components/user-form/user-form.component-module.ts
@@ -8,10 +8,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 import { UserFormComponent } from './user-form.component';
 
 @NgModule({
-  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, CommonModule, MatButtonModule, MatAutocompleteModule, MatOptionModule, MatSelectModule],
+  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, CommonModule, MatButtonModule, MatAutocompleteModule, MatOptionModule, MatSelectModule, MatCheckboxModule, MatRadioModule],
   declarations: [UserFormComponent],
   providers: [],
   exports: [UserFormComponent]
