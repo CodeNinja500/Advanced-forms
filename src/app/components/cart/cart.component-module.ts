18f5f2d2dfbbb693ac050dec5bc586a299c57fde
Maciej Paluch <maciekpal@gmail.com>
2022-11-29 21:25:51 +0100

    DatePicker fixed

--- src/app/components/cart/cart.component-module.ts
+++ src/app/components/cart/cart.component-module.ts
@@ -7,9 +7,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { CartComponent } from './cart.component';
 import {MatInputModule} from "@angular/material/input";
 import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
-  imports: [ReactiveFormsModule, MatCardModule, MatCheckboxModule, CommonModule, MatButtonModule, MatInputModule, MatDatepickerModule],
+  imports: [ReactiveFormsModule, MatCardModule, MatCheckboxModule, CommonModule, MatButtonModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
   declarations: [CartComponent],
   providers: [],
   exports: [CartComponent]
